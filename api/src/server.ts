400e4ff6175d561ce33757e66a0d31e5fcbc9e1a
Diego C. Rodrigues <diegocrodrigues.dev@gmail.com>
2021-01-17 16:40:17 -0300

    Cors

--- api/src/server.ts
+++ api/src/server.ts
@@ -4,12 +4,13 @@ import bodyParser from 'body-parser';
 import favicon from 'serve-favicon';
 import path from 'path';
 import mongoose from 'mongoose';
+import cors from 'cors';
 import routes from './routes';
 import { PORT, HOST, MONGO_URI } from './utils/config';
 
 const app = express();
 const DateNow = Date().toString();
-
+app.use(cors())
 app.use(bodyParser.json());
 
 app.use(favicon(path.join(`${__dirname}/public/favicon.ico`)));
